d3d825d9c5e13eba539f407d1c8c8055d9aef33f
Milton Figueredo Miles <gratno@gmail.com>
2022-09-13 20:37:18 -0500

    Java
    aprendo

---
+++ PrimerProyectoWhile/PrimeraClase.java
@@ -0,0 +1,22 @@
+/** Aprendo*/
+public class PrimeraClase
+{
+    public static void main (String[] parametroR){
+        
+           /*int i = 1;
+           //while  == mientras
+            while(i < 1){
+            
+            System.out.println(i);
+            i++;
+            }*/
+            
+            int j = 1;
+            // do hacer o hacer lo siguente
+            do{
+            
+                System.out.println(j);
+                j++;
+            }while(j < 1);
+    }
+}
